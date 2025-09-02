b1f9ef64e1c358dfb52b6999c61333aab1c8d61d
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-09-01 23:43:23 -0300

    feat: :seedling: Controller

--- src/main/java/com/zhant/entregasGestor/controllers/EntregaController.java
+++ src/main/java/com/zhant/entregasGestor/controllers/EntregaController.java
@@ -2,18 +2,45 @@ package com.zhant.entregasGestor.controllers;
 
 import java.util.List;
 
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.zhant.entregasGestor.models.Entrega;
 import com.zhant.entregasGestor.services.EntregaService;
+
+import jakarta.validation.Valid;
 
 public class EntregaController {
 
 	@Autowired
 	EntregaService entregaService;
 	
+	@GetMapping
 	public List<Entrega> findEntregas(){
 		return entregaService.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Entrega create(@Valid @RequestBody Entrega entrega) {
+		return entregaService.create(entrega);
+	}
+	
+	@PutMapping("/{id}")
+	public Entrega update(@Valid @RequestBody Entrega entrega, int id) throws BadRequestException {
+		return entregaService.update(id, entrega);
+	}
+	
+	@DeleteMapping
+	public void delete(@Valid @RequestBody Entrega entrega, int id) throws BadRequestException {
+		entregaService.delete(id);
 	}
 	
 }
