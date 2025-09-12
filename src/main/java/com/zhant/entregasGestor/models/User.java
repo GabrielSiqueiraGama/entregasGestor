747c53ba8d68a41a30967f6e1c12facd12000e26
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-09-11 23:52:17 -0300

    feat: :seedling: Adding UserDetails

---
+++ src/main/java/com/zhant/entregasGestor/models/User.java
@@ -0,0 +1,66 @@
+package com.zhant.entregasGestor.models;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("serial")
+@Entity(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails{
+/*	id TEXT PRIMARY KEY UNIQUE NOT NULL,
+	login TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL
+	role TEXT NOT NULL*/
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("_id")
+	private int id;
+    @NotNull
+	private String login;
+    @NotNull
+	private String password;
+    @NotNull
+	private UserRole role;
+    
+    public User(String login, String password, UserRole role) {
+    	this.login = login;
+    	this.password = password;
+    	this.role = role;
+    }
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));//Caso seja admin, vai ter os direitos de admnin e de usuario também.
+		else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return login;
+	}
+	public User(int id, @NotNull String login, @NotNull String password, @NotNull UserRole role) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.password = password;
+		this.role = role;
+	}
+	public User() {
+		super();
+	}
+} 
