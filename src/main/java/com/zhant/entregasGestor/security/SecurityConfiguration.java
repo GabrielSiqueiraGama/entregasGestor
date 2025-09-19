317aca54c79180bf17ed4acfc6b0c17fe781c6fd
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-09-19 01:10:52 -0300

    fix: :wrench: fixing to test fetch on mobile application

--- src/main/java/com/zhant/entregasGestor/security/SecurityConfiguration.java
+++ src/main/java/com/zhant/entregasGestor/security/SecurityConfiguration.java
@@ -30,6 +30,7 @@ public class SecurityConfiguration {
 				.authorizeHttpRequests(authorize -> authorize
 						.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/deliveries").permitAll()
                         .requestMatchers(HttpMethod.POST, "/deliveries").hasRole("ADMIN")
                         .anyRequest().authenticated())
 				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
